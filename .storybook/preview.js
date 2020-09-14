7a3a3141f45533119d518e1d943287db3107803d
alalay <alalay888@gmail.com>
2020-09-14 13:54:06 +0800

    add global style

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import React from "react";
 
+import { GlobalStyle } from "../src/utils/global";
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  )
+];
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: "^on[A-Z].*" }
+};
