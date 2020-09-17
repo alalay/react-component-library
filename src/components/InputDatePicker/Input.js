2cdd1b97cf2dc66f5009183d31d227a214b190c7
alalay <alalay888@gmail.com>
2020-09-17 09:40:32 +0800

    use DebounceInput

--- src/components/InputDatePicker/Input.js
+++ src/components/InputDatePicker/Input.js
@@ -1,9 +1,17 @@
 import React, { useContext, forwardRef } from "react";
+import DebounceInput from "react-debounce-input";
 import DateContext from "./Context";
 
 function Input(props, ref) {
   const { value, onInputChange } = useContext(DateContext);
-  return <input value={value.textInput} onChange={onInputChange} ref={ref} />;
+  return (
+    <DebounceInput
+      debounceTimeout={300}
+      value={value.textInput}
+      onChange={onInputChange}
+      ref={ref}
+    />
+  );
 }
 
 export default forwardRef(Input);
